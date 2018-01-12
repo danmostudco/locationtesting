9913cd0a808d0e03cde79202bc9c7d4e08be1c8d
Dan Morrison <morrison.danr@gmail.com>
2018-01-12 11:58:16 -0500

    create flexbox inside panel with two columns that hold the values displayed

--- src/App.js
+++ src/App.js
@@ -3,47 +3,58 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-
+    return <div className="App">
         <div className="Grid">
+          <div className="Grid-cell">
+            <h2>
+              Low Power <span>.getCurrentPosition</span>
+            </h2>
+            <h3>
+              One time ping to get your geolocation. The results will be
+              displayed below.
+            </h3>
+            <p className="label">Latitude</p>
+            <p>38.9102177</p>
+
+            <p className="label">Longitude</p>
+            <p>-77.0100023</p>
+
+            <p className="label">Accuracy</p>
+            <p>20 meters</p>
+
+            <p className="label">Time</p>
+            <p>3.13 seconds</p>
+          </div>
+
           <div className="Grid-cell">
             <h2>Low Power <span>.getCurrentPosition</span></h2>
             <h3>One time ping to get your geolocation. The results will be displayed below.</h3>
-            <p className="label">Latitude</p>
-            <p>38.9102177</p>
+            <div className="panelContainer">
+              <div className="panelColumn">
+                <div className="valuePair">
+                  <p className="label">Latitude</p>
+                  <p>38.9102177</p>
+                </div>
+                <div className="valuePair">
+                  <p className="label">Time</p>
+                  <p>3.13 <span className="miniText">seconds</span></p>
+                </div>
+              </div>
 
-            <p className="label">Longitude</p>
-            <p>-77.0100023</p>
-
-            <p className="label">Accuracy</p>
-            <p>20 meters</p>
-
-            <p className="label">Time</p>
-            <p>3.13 seconds</p>
-
-          </div>
-
-
-          <div className="Grid-cell">
-          <h2>Low Power <span>.getCurrentPosition</span></h2>
-            <h3>One time ping to get your geolocation. The results will be displayed below.</h3>
-            <p className="label">Latitude</p>
-            <p>38.9102177</p>
-
-            <p className="label">Longitude</p>
-            <p>-77.0100023</p>
-
-            <p className="label">Accuracy</p>
-            <p>20 meters</p>
-
-            <p className="label">Time</p>
-            <p>3.13 seconds</p>
+              <div className="panelColumn">
+                <div className="valuePair">
+                  <p className="label">Longitude</p>
+                  <p>-77.0100023</p>
+                </div>
+                <div className="valuePair">
+                  <p className="label">Accuracy</p>
+                  <p>20 <span className="miniText">meters</span></p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
-      </div>
-    );
+      </div>;
   }
 }
 
